fcd8a06489a24b7c27f84e57e16334445e4cec00
Harsh Pal <excalibur@Harshs-MacBook-Pro.local>
2020-08-27 21:12:16 +0530

    Breakpoint-2

--- UI_Extensions/activity/background.js
+++ UI_Extensions/activity/background.js
@@ -1,1 +1,56 @@
 console.log("Hello from the background file");
+let blockList = [];
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+     let newEntry = request;
+     console.log(newEntry);
+     blockList.push ({site : newEntry, time :10});
+     console.log(sender);
+     sendResponse(true);
+});
+
+//polling
+//tracker
+async function init () {
+     if (blockList.length > 0){
+           //active tab request
+           let tab = await getTab();
+           console.log(tab);
+           if(tab) {
+                 let cURL = tab.url;
+                 for(let i=0; i<blockList.length;i++){
+                     let isUsing = (cURL).includes(blockList[i].site);
+                       if(isUsing){
+                            blockList[i].time--;
+                            chrome.browserAction.setBadgeText({ text: blockList[i].time + "" });//timer next to extention icon
+                            //console.log(blockList[i].time);
+                            if(blockList[i].time <= 0){
+                                     chrome.browserAction.setBadgeText({ text: blockList[i].time + "" });
+                                     await removeTab(tab);
+                                     console.log("close tab");
+                            }
+                       }
+                 }
+           }
+           //loop block list
+           //decreament time
+           //block website
+     }
+     console.log("polling");
+}
+//timer
+setInterval(init, 1000);
+function getTab() {
+      return new Promise (function (resolve, reject) {
+           chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                resolve(tabs[0]);
+            })
+      })
+}
+
+function removeTab (tab) {
+      return new Promise (function (resolve, reject) {
+            chrome.tabs.remove(tab.id, function () {
+                  resolve();
+            })
+      })
+}
--- UI_Extensions/activity/popup.js
+++ UI_Extensions/activity/popup.js
@@ -1,1 +1,31 @@
 console.log("hello from popup.js");
+let input = document.querySelector("input");
+let ul = document.querySelector("ul");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click",async function () {
+     //send message
+     let tobeBlocked = input.value;
+     if(tobeBlocked){
+          await sendMessage(tobeBlocked);
+          let li = document.createElement("li");
+          li.setAttribute("class", "list-group-item");
+          li.innerHTML = tobeBlocked + '<i class="fas fa-times>"</i>';
+          ul.appendChild(li);
+          input.value = '';
+
+          let i = li.querySelector("i");
+          i.addEventListener("click", function () {
+             i.parentNode.remove(); 
+          })
+     }
+})
+
+//popup
+function sendMessage (tobeBlocked) {
+     return new Promise ( function(resolve, reject) {
+         chrome.runtime.sendMessage(tobeBlocked, function (response) {
+              resolve(response);
+         });
+     })
+}
